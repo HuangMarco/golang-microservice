fd89d6ad2117a25c516bebbd6f90876633ce6b6b
HuangMarco <ht576475012@163.com>
2021-11-24 18:24:35 +0800

    [update] updated for webserver.go

--- goblog/accountservice/service/webserver.go
+++ goblog/accountservice/service/webserver.go
@@ -4,20 +4,17 @@ import (
 	"log"
 	"net/http"
 	"fmt"
+	"os"
 )
 
 //new handler
 type apiHanlder struct {}
 //等效于func (apiHanlder) ServeHTTP(w http.ResponseWriter, r *http.Request)
-// 为结构体apiHanlder添加ServeHTTP方法
+//为结构体apiHanlder添加ServeHTTP方法
 func (handler apiHanlder) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Welcome to the %v page!",r.URL.Path)
 
 }
-
-
-
-
 
 func StartWebServer(port string) error {
 
@@ -33,10 +30,17 @@ func StartWebServer(port string) error {
 	return nil
 }
 
+func forTestAPI(path string){
+	log.Printf("The request is for path: %v", path)
+	fmt.Fprintf(os.Stdout, "Do something for /test api request.\n")
+}
+
 // How to use ServeMux: https://pkg.go.dev/net/http#ServeMux.Handle
 func RequestHandler(port string) http.Handler {
 	log.Println("The requested port is:", port)
+	//创建一个新的ServeMux来处理restful api 
 	mux := http.NewServeMux()
+	//单独为/api/添加一个handler，而不会再交给HandleFunc
 	mux.Handle("/api/", apiHanlder{})
 	mux.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
 		// The "/" pattern matches everything, so we need to check
@@ -44,8 +48,12 @@ func RequestHandler(port string) http.Handler {
 		log.Printf("The request url is: %v", r.URL.Path)
 		if r.URL.Path == "/test" {
 			// log.Printf("Welcome to the %v page",r.URL.Path)
+			//fmt.Fprintf将会将内容输出到输出流中:https://pkg.go.dev/fmt#Fprintf
 			fmt.Fprintf(w, "Welcome to the %v page!",r.URL.Path)
+			forTestAPI(r.URL.Path)
+			//此处退出，而不会将server暂停，只是针对于/test请求的处理结束
 			return
+
 		}else if r.URL.Path == "/api/"{
 			fmt.Fprintf(w, "Welcome to the %v page!",r.URL.Path)
 			return
